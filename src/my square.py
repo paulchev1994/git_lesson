67c8674f2b6f3bdf95039356a5f3afa30264a59c
Paul Chevalier <paulchev@gmail.com>
2017-10-23 18:13:18 -0400

    aded and udpated doco

--- src/my square.py
+++ src/my square.py
@@ -1,9 +1,12 @@
 def my_square(x):
 	""" takes a value and returns the square
+	uses **
 	 you know what square does
 	"""
 	return(x **2)
 def my_square2(x):
+	""" used the *  operator
+	"""
 	return (x * x)
 
 print(my_square(4))
